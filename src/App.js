c29eef7256efcaaf4ead5542ad10e7b6a7db3aee
zenjutahi <jeffmnjogu@gmail.com>
2020-01-20 12:30:00 +0300

    [chore] styles patterns in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import "./App.css"
+import * as styles from "./styles"
+
 class App extends Component {
 
   render() {
     const  name = "zenj"
+
     return (
       <div className="App">
         <button>{name}</button>
+        <div style={styles.style}>
+          Style
+        </div>
       </div>
     )
   }
