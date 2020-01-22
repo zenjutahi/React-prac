8aef1d5b86ec9e587c7a95aa57499502fce4e951
zenjutahi <zenjutahi@gmail.com>
2020-01-22 16:39:19 +0300

    [chore] working with maping items in a list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Component1 from './functional/component1'
-import Container1 from './containers/container1'
+import Container3 from './containers/container3'
 import Container2 from './containers/container2'
 import "./App.css"
 import { style } from "./styles"
@@ -18,7 +18,8 @@ class App extends Component {
           Style
         </div>
         <hr />
-        <Container2 nickname="G!, to display after state change"/>
+        <Container2 />
+        <Container3 />
       </div>
     )
   }
