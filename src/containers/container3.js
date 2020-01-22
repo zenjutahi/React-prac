12d1c437614f278801053466a7c93219c2f0a786
zenjutahi <zenjutahi@gmail.com>
2020-01-22 16:39:26 +0300

    [chore] working with maping items in a list

---
+++ src/containers/container3.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Container3 extends Component {
+
+  state = {
+    value: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state.value)
+  }
+
+  render () {
+    return (
+      <div>
+        <p>Current state: {this.state.value}</p>
+        <form onSubmit={this.handleSubmit}>
+          <label> Name </label>
+          <input id="word" onChange={this.handleChange} type="text" />
+          <button type="submit"> Submit </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Container3;
